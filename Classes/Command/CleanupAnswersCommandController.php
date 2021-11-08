d404ad40a88cb6c76fb5f3a236ade41f868e7148
Stefan Busemann <stefan.busemann@in2code.de>
2021-11-08 15:20:00 +0100

    [FEATURE] Allow to clean multiple pages

--- Classes/Command/CleanupAnswersCommandController.php
+++ Classes/Command/CleanupAnswersCommandController.php
@@ -29,26 +29,29 @@ class CleanupAnswersCommandController extends CommandController
     }
     /**
      * @param int $age Age of the answers in seconds. e.g. 5184000 = 60 days
-     * @param int $pid Optional PID. If set, only answers stored on the given PID are cleaned up.
+     * @param string $pid Optional PID. If set, only answers stored on the given PID are cleaned up.
      * @return bool
      */
-    public function deleteCommand(int $age, int $pid = null): bool
+    public function deleteCommand(int $age, string $pid = null): bool
     {
         $configurationManager = GeneralUtility::makeInstance(ConfigurationManager::class);
 
+        $pids = GeneralUtility::trimExplode(',', $pid, true);
         $this->settings = $configurationManager->getConfiguration(
             ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
         $this->cleanupService->settings = $this->settings['module.']['tx_powermail.']['settings.'];
+        foreach ($pids as $pidToClean) {
+            $stats = $this->cleanupService->deleteMailsOlderThanAgeInPid($age, (int) $pidToClean);
+            $this->outputLine(
+                sprintf(
+                    'Deleted %d mails containing %d answers and %d files',
+                    $stats['mails'],
+                    $stats['answers'],
+                    $stats['files']
+                )
+            );
+        }
 
-        $stats = $this->cleanupService->deleteMailsOlderThanAgeInPid($age, $pid);
-        $this->outputLine(
-            sprintf(
-                'Deleted %d mails containing %d answers and %d files',
-                $stats['mails'],
-                $stats['answers'],
-                $stats['files']
-            )
-        );
         return true;
     }
 }
