e190b499e34bfad098474f6b81e591156ff3f393
Andreas Nedbal <andreas.nedbal@in2code.de>
2023-05-04 16:33:43 +0200

    [TASK] Add flexform override functionality
    Related: https://projekte.in2code.de/issues/55709

---
+++ Classes/Hooks/OverwriteFlexForm.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace In2code\PowermailCleaner\Hooks;
+
+/**
+ * Class OverwriteFlexForm
+ */
+class OverwriteFlexForm
+{
+    /**
+     * @var string
+     */
+    protected $path = 'FILE:EXT:powermail_cleaner/Configuration/FlexForms/FlexformPi1.xml';
+
+    /**
+     * @return void
+     */
+    public function overwrite()
+    {
+        $GLOBALS['TCA']['tt_content']['columns']['pi_flexform']['config']['ds']['in2studyfinder_pi1,list']
+            = $this->path;
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -9,4 +9,7 @@ call_user_func(function () {
      */
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] =
         \In2code\PowermailCleaner\Command\CleanupAnswersCommandController::class;
+
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp'][] =
+        \In2code\PowermailCleaner\Hooks\OverwriteFlexForm::class . '->overwrite';
 });
