fa54e794d0827c0d6cba26b13f4210372603844a
Andreas Nedbal <andreas.nedbal@in2code.de>
2023-05-04 14:54:18 +0200

    [TASK] Raise package versions to v9 and add base development setup with DDEV

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.1.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.99.99',
+            'typo3' => '9.5.0-9.5.99',
             'powermail' => '6.2.0-7.4.99',
         ],
         'conflicts' => [
