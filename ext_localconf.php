6d5dcaf3748b633d3e888934d0a1e045f58f75b9
Andreas Nedbal <andreas.nedbal@in2code.de>
2023-05-09 12:04:29 +0200

    [TASK] Show data deletion behaviour in plugin preview
    Related: https://projekte.in2code.de/issues/55706

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,4 +12,8 @@ call_user_func(function () {
 
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][]
         = \In2code\PowermailCleaner\Hooks\FlexFormHook::class;
+
+    $cmsLayout = 'cms/layout/class.tx_cms_layout.php';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][$cmsLayout]['tt_content_drawItem']['powermail'] =
+        \In2code\PowermailCleaner\Hooks\PluginPreview::class;
 });
