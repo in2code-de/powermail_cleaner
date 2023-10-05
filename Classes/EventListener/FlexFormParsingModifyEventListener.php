d0c0d8d5b0d679fa6afcec948c16ce4371595082
Marcus Schwemer <marcus.schwemer@in2code.de>
2023-10-05 10:18:08 +0200

    [CLEANUP] Remove unused code
    https://projekte.in2code.de/issues/59412

--- Classes/EventListener/FlexFormParsingModifyEventListener.php
+++ Classes/EventListener/FlexFormParsingModifyEventListener.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace In2code\PowermailCleaner\EventListener;
 
-use In2code\Powermail\Utility\DatabaseUtility;
-use Psr\Http\Message\ServerRequestInterface;
-use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Configuration\Event\AfterFlexFormDataStructureParsedEvent;
-use TYPO3\CMS\Core\TypoScript\TypoScriptService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 final class FlexFormParsingModifyEventListener
@@ -28,53 +24,5 @@ final class FlexFormParsingModifyEventListener
             }
             $event->setDataStructure($parsedDataStructure);
         }
-    }
-
-    /**
-     * Get field configuration from page TSconfig
-     *
-     * @param int $pid Record pid
-     * @return array
-     */
-    protected function getFieldConfiguration(): array
-    {
-        $pid = $this->getPidForCurrentRecord();
-
-        $tsConfiguration = BackendUtility::getPagesTSconfig((int)$pid);
-        if (!empty($tsConfiguration['tx_powermail.']['flexForm.']['addField.'])) {
-            $eConfiguration = $tsConfiguration['tx_powermail.']['flexForm.']['addField.'];
-            $tsService = GeneralUtility::makeInstance(TypoScriptService::class);
-            return $tsService->convertTypoScriptArrayToPlainArray($eConfiguration);
-        }
-        return [];
-    }
-
-    /**
-     * Get sheetname and remove from configuration array
-     *
-     * @param array $configuration
-     * @return string
-     */
-    protected function getSheetNameAndRemoveFromConfiguration(array &$configuration): string
-    {
-        $sheet = $this->allowedSheets[0];
-        if (!empty($configuration['_sheet']) && in_array($configuration['_sheet'], $this->allowedSheets)) {
-            $sheet = $configuration['_sheet'];
-        }
-        unset($configuration['_sheet']);
-        return $sheet;
-    }
-
-    private function getPidForCurrentRecord(): int
-    {
-        $request = $this->getRequest();
-        $queryParams = $request->getQueryParams();
-        $uid = array_keys($queryParams['edit']['tt_content'] ?? [])[0] ?? 0;
-        return DatabaseUtility::getPidForRecord($uid, 'tt_content');
-    }
-
-    private function getRequest(): ServerRequestInterface
-    {
-        return $GLOBALS['TYPO3_REQUEST'];
     }
 }
