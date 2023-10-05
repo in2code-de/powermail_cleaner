6f2e6e3f00c05e05a76ae6d1b6f116951b4372a4
Marcus Schwemer <marcus.schwemer@in2code.de>
2023-10-05 11:55:15 +0200

    [CLEANUP] Remove dead code
    https://projekte.in2code.de/issues/59412

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,15 +6,11 @@ call_user_func(function () {
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][]
         = \In2code\PowermailCleaner\Hooks\FlexFormHook::class;
 
-    $cmsLayout = 'cms/layout/class.tx_cms_layout.php';
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][$cmsLayout]['tt_content_drawItem']['powermail'] =
-        \In2code\PowermailCleaner\Hooks\PluginPreview::class;
-
-    $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
-    $signalSlotDispatcher->connect(
-        \In2code\Powermail\Controller\FormController::class,  // Signal class name
-        'createActionAfterMailDbSaved',                                  // Signal name
-        \In2code\PowermailCleaner\Hooks\AfterMailSave::class,        // Slot class name
-        'attachPlugin'                               // Slot name
-    );
+//    $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
+//    $signalSlotDispatcher->connect(
+//        \In2code\Powermail\Controller\FormController::class,  // Signal class name
+//        'createActionAfterMailDbSaved',                                  // Signal name
+//        \In2code\PowermailCleaner\Hooks\AfterMailSave::class,        // Slot class name
+//        'attachPlugin'                               // Slot name
+//    );
 });
