335740fe167620f0bc5e032b39d51ec91a07f290
Marcus Schwemer <marcus.schwemer@in2code.de>
2023-10-09 12:08:48 +0200

    [CLEANUP] Remove dead code
    https://projekte.in2code.de/issues/59412

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,8 +3,6 @@
 defined('TYPO3') || die('Access denied.');
 
 call_user_func(function () {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][]
-        = \In2code\PowermailCleaner\Hooks\FlexFormHook::class;
 
 //    $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
 //    $signalSlotDispatcher->connect(
