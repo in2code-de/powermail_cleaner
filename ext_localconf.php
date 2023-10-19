8c1c37a7fb849da40c383fc035926095e94d5e36
Marcus Schwemer <marcus.schwemer@in2code.de>
2023-10-19 17:13:09 +0200

    [FEATURE] Cleanup mails based on their stored deletion date
    Resolves in2code-pro/powermail_cleaner#3
    Resolves in2code-pro/powermail_cleaner#5
    Related https://projekte.in2code.de/issues/57189

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,5 +3,11 @@
 defined('TYPO3') || die('Access denied.');
 
 call_user_func(function () {
-
+    $GLOBALS['TYPO3_CONF_VARS']['LOG']['In2code']['PowermailCleaner']['Command']['CleanupCommand']['writerConfiguration'] = [
+        \Psr\Log\LogLevel::INFO => [
+            \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [
+                'logFile' => \TYPO3\CMS\Core\Core\Environment::getVarPath() . '/log/powermail-cleaner_7ac500bce5.log'
+            ],
+        ]
+    ];
 });
--- ext_tables.php
+++ ext_tables.php
@@ -7,9 +7,11 @@ call_user_func(
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\In2code\Powermail\Controller\FormController::class] = [
             'className' => \In2code\PowermailCleaner\Controller\FormController::class
         ];
-
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\In2code\Powermail\Domain\Model\Mail::class] = [
             'className' => \In2code\PowermailCleaner\Domain\Model\Mail::class
+        ];
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\In2code\Powermail\Domain\Repository\MailRepository::class] = [
+            'className' => \In2code\PowermailCleaner\Domain\Repository\MailRepository::class
         ];
     }
 );
