ec1bda7fed80e36da418ddcabf9686c07c09d087
Marcus Schwemer <marcus.schwemer@in2code.de>
2023-10-19 15:09:14 +0200

    [FEATURE] Save deletion_timestamp with sent e-mails
    Resolves in2code-pro/powermail_cleaner#5
    Related https://projekte.in2code.de/issues/57189

--- Classes/EventListener/FormControllerCreateActionBeforeRenderViewEventListener.php
+++ Classes/EventListener/FormControllerCreateActionBeforeRenderViewEventListener.php
@@ -8,6 +8,9 @@ use In2code\Powermail\Events\FormControllerCreateActionBeforeRenderViewEvent;
 use In2code\Powermail\Utility\HashUtility;
 use In2code\PowermailCleaner\Controller\FormController;
 use In2code\PowermailCleaner\Domain\Model\Mail;
+use TYPO3\CMS\Core\Service\FlexFormService;
+use TYPO3\CMS\Core\Utility\ArrayUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 final class FormControllerCreateActionBeforeRenderViewEventListener
 {
@@ -20,10 +23,37 @@ final class FormControllerCreateActionBeforeRenderViewEventListener
         $hash = $event->getHash();
 
         $mail->setPlugin($controller->getContentObject()->data['uid']);
+        $mail->setDeletionTimestamp($this->calculateDeletionTimeStamp($controller));
+
         $event->setMail($mail);
 
         if ($hash !== '') {
             $event->setHash(HashUtility::getHash($mail));
         }
+    }
+
+    private function calculateDeletionTimeStamp(FormController $controller): int
+    {
+        $settings = $controller->getSettings();
+        if (ArrayUtility::isValidPath($settings, 'powermailCleaner')) {
+            switch ($settings['powermailCleaner']['deletionBehavior']) {
+                case 'deletionPeriod':
+                    return $this->calculateDeletionTimeStampForPeriod(
+                        (int)$settings['powermailCleaner']['deletionPeriod']
+                    );
+                case 'deletionDate':
+                    return (int)$settings['powermailCleaner']['deletionDate'];
+                case 'dbDisable':
+                    return -1;
+                default:
+                    return 0;
+            }
+        }
+        return 0;
+    }
+
+    private function calculateDeletionTimeStampForPeriod(int $deletionPeriod): int
+    {
+        return time() + $deletionPeriod * 86400;
     }
 }
--- Configuration/TCA/Overrides/tx_powermail_domain_model_mail.php
+++ Configuration/TCA/Overrides/tx_powermail_domain_model_mail.php
@@ -5,6 +5,16 @@ defined('TYPO3') || die('Access denied.');
 use In2code\Powermail\Domain\Model\Mail;
 
 $columns = [
+    'deletion_timestamp' => [
+        'exclude' => 1,
+        'label' => 'LLL:EXT:powermail_cleaner/Resources/Private/Language/locallang_db.xlf:mail.deletionDate',
+        'config' => [
+            'type' => 'datetime',
+            'format' => 'datetime',
+            'eval' => 'int',
+            'readOnly' => true,
+        ],
+    ],
     'plugin' => [
         'exclude' => 1,
         'label' => 'LLL:EXT:powermail_cleaner/Resources/Private/Language/locallang_db.xlf:mail.related-plugin',
@@ -17,4 +27,4 @@ $columns = [
 ];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(Mail::TABLE_NAME, $columns);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(Mail::TABLE_NAME, 'plugin', '', 'after:form');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(Mail::TABLE_NAME, 'deletion_timestamp,plugin', '', 'after:crdate');
