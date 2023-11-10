45827e8786b0c394514746b5c6c8c0f6fa7f9d4e
Stefan Busemann <stefan.busemann@in2code.de>
2023-11-10 18:48:43 +0100

    [FEATURE] Display releated plugin in mail record
    https://projekte.in2code.de/issues/59412

--- Configuration/TCA/Overrides/tx_powermail_domain_model_mail.php
+++ Configuration/TCA/Overrides/tx_powermail_domain_model_mail.php
@@ -6,17 +6,14 @@ use In2code\Powermail\Domain\Model\Mail;
 $columns = [
     'plugin' => [
         'exclude' => 1,
-        'label' => 'LLL:EXT:powermail_cleaner/Resources/Private/Language/locallang_db.xlf:' . Mail::TABLE_NAME . '.form',
+        'label' => 'LLL:EXT:powermail_cleaner/Resources/Private/Language/locallang_db.xlf:mail.related-plugin',
         'config' => [
-            'type' => 'input',
-            'size' => 40,
+            'type' => 'inline',
+            'foreign_table' => 'tt_content',
             'readOnly' => true,
-            'eval' => 'num,trim',
         ],
     ]
 ];
 
-$GLOBALS['TCA'][Mail::TABLE_NAME]['columns'] = array_merge(
-    $GLOBALS['TCA'][Mail::TABLE_NAME]['columns'],
-    $columns
-);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(Mail::TABLE_NAME, $columns);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(Mail::TABLE_NAME, 'plugin', '', 'after:form');
