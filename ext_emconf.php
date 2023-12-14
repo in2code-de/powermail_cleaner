85090fc6548796b043d908900a9a0a5455755c74
Marcus Schwemer <marcus.schwemer@in2code.de>
2023-12-14 09:11:10 +0100

    [TASK] EXT:powermail_cleaner :: Release 5.0.0
    Release the first tagged version of powermail_cleaner for EXT:powermail
    v12 and TYPO3 v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,11 +9,11 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '5.0.0-dev',
+    'version' => '5.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-12.4.99',
-            'powermail' => '12.0.0-12.0.99',
+            'powermail' => '12.0.0-12.9.99',
         ],
         'conflicts' => [
         ],
