937e96b0ac805d4a9d5b90291c735ab2744f25c7
Stefan Busemann <stefan.busemann@in2code.de>
2024-07-19 16:06:08 +0200

    [FEATURE] Disable frontend of powermail cleaner
    by setting the typoscript configuration   settings.setup.powermail_cleaner_enabled =0 and disabling the templates, partials and layouts, you can disable powermail cleaner in the Frontend. This is helpful, if you want to prepare the cleaner settings in a running installation
    
    Example:
    
    plugin.tx_powermail.view.templateRootPaths.10 >
    plugin.tx_powermail.view.partialRootPaths.10 >
    plugin.tx_powermail.view.layoutRootPaths.10 >
    plugin.tx_powermail.settings.setup.powermail_cleaner_enabled = 1

--- Classes/Controller/FormController.php
+++ Classes/Controller/FormController.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace In2code\PowermailCleaner\Controller;
@@ -9,26 +10,38 @@ use TYPO3\CMS\Core\Messaging\FlashMessageService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
-class FormController extends \In2code\Powermail\Controller\FormController {
+class FormController extends \In2code\Powermail\Controller\FormController
+{
     /**
      * @return ContentObjectRenderer
      */
-    public function getContentObject() {
+    public function getContentObject()
+    {
         return $this->contentObject;
     }
+
     public function initializeFormAction()
     {
-        if (empty($this->settings['powermailCleaner']['deletionBehavior'])) {
-            $message = GeneralUtility::makeInstance(FlashMessage::class,
-                LocalizationUtility::translate('LLL:EXT:powermail_cleaner/Resources/Private/Language/locallang_db.xlf:pluginInfo.noCleanerConfiguration.message'),
-                LocalizationUtility::translate('LLL:EXT:powermail_cleaner/Resources/Private/Language/locallang_db.xlf:pluginInfo.noCleanerConfiguration.title'),
-                FlashMessage::ERROR,
-                true
-            );
+        if (1 == 1) {
+            if (empty($this->settings['powermailCleaner']['deletionBehavior']) && ($this->settings['powermail_cleaner_enabled'] == 1)) {
+                $message = GeneralUtility::makeInstance(
+                    FlashMessage::class,
+                    LocalizationUtility::translate(
+                        'LLL:EXT:powermail_cleaner/Resources/Private/Language/locallang_db.xlf:pluginInfo.noCleanerConfiguration.message'
+                    ),
+                    LocalizationUtility::translate(
+                        'LLL:EXT:powermail_cleaner/Resources/Private/Language/locallang_db.xlf:pluginInfo.noCleanerConfiguration.title'
+                    ),
+                    FlashMessage::ERROR,
+                    true
+                );
 
-            $flashMessageService = GeneralUtility::makeInstance(FlashMessageService::class);
-            $messageQueue = $flashMessageService->getMessageQueueByIdentifier('extbase.flashmessages.tx_powermail_pi1');
-            $messageQueue->addMessage($message);
+                $flashMessageService = GeneralUtility::makeInstance(FlashMessageService::class);
+                $messageQueue = $flashMessageService->getMessageQueueByIdentifier(
+                    'extbase.flashmessages.tx_powermail_pi1'
+                );
+                $messageQueue->addMessage($message);
+            }
         }
     }
 
