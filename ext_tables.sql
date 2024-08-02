1b6ed57e63a9238d55ee8ad9d8289b3e5de3e084
Stefan Busemann <stefan.busemann@in2code.de>
2024-08-02 18:33:27 +0200

    [BUGFIX] Add missing SQL Statement

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,5 +2,6 @@
 # Table structure for table 'tx_powermail_domain_model_mail'
 #
 CREATE TABLE tx_powermail_domain_model_mail (
-    plugin int(11) DEFAULT '0' NOT NULL
+    plugin int(11) DEFAULT '0' NOT NULL,
+    deletion_timestamp int(11) unsigned DEFAULT '0' NOT NULL,
 );
