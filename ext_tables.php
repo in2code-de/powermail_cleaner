cbcdaa4e379f1eb8dbd9c69c2373882e5f96d355
Stefan Busemann <stefan.busemann@in2code.de>
2024-09-17 14:14:48 +0200

    [BUGFIX] wrong classname for backend module

--- ext_tables.php
+++ ext_tables.php
@@ -4,8 +4,7 @@ if (!defined('TYPO3_MODE')) {
 }
 call_user_func(
     function () {
-        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions']['Powermail']['modules']['web_PowermailM1']['controllers']['Module']['actions'][] = 'cleanup';
-
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions']['Powermail']['modules']['web_PowermailM1']['controllers']['In2code\Powermail\Controller\ModuleController']['actions'][] = 'cleanup';
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\In2code\Powermail\Controller\ModuleController::class] = [
             'className' => \In2code\PowermailCleaner\Controller\ModuleController::class
         ];
