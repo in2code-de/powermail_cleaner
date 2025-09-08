c000b9b0181cab86b77e662bb28b6960d2e743b2
Marcus Schwemer <marcus.schwemer@in2code.de>
2025-09-08 10:47:09 +0200

    [TASK] Move extension icon to the correct place
    https://projekte.in2code.de/issues/73935

---
+++ Configuration/Icons.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'in2code-powermail-cleaner' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:powermail_cleaner/Resources/Public/Icons/Extension.svg',
+    ],
+];
